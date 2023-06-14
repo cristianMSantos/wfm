4fd90a22f5d7bf147f6bdabe83ac1c418a7ec545
cristian_santos <cristian_santos@git>
2023-06-14 17:08:39 -0300

    add_getuser

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { styled, useTheme } from '@mui/material/styles';
+import { useSelector, useDispatch } from 'react-redux'
 import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,21 +9,40 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { SidebarContext } from "../App";
+import api from '../axios'
+import { setUser } from '../store/features/User'
 
 
 export default function NavBar() {
-    const sidebarProps  = useContext(SidebarContext);
-    const {setOpen}  = useContext(SidebarContext);
+    const sidebarProps = useContext(SidebarContext);
+    const { setOpen } = useContext(SidebarContext);
     const { drawerWidth, open } = sidebarProps;
 
+    const getUser = async () => {
+        const options = {
+            url: `/auth/me`,
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+        return await api(options)
+            .then((response) => {
+                dispatch(setUser(response.data))
+            })
+            .catch((error) => {
+                console.error('Erro ao buscar o usuário:', error.response);
+            })
+    }
+
     useEffect(() => {
-        console.log(open)
-        console.log(drawerWidth)
-    }, [sidebarProps])
+        console.log('aqui')
+        getUser()
+    }, [])
 
     const handleDrawerOpen = () => {
         setOpen(true);
-      };
+    };
 
     const AppBar = styled(MuiAppBar, {
         shouldForwardProp: (prop) => prop !== 'open',
--- src/store/features/User.js
+++ src/store/features/User.js
@@ -1,27 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 
-export const login = createSlice({
-    name: 'login',
+export const user = createSlice({
+    name: 'user',
     initialState: {
-        isAuthenticated: localStorage.getItem('token'),
+        user: null,
     },
     reducers: {
-        setLogin: (state, {payload}) => {
+        setUser: (state, {payload}) => {
             // Redux Toolkit allows us to write "mutating" logic in reducers. It
             // doesn't actually mutate the state because it uses the Immer library,
             // which detects changes to a "draft state" and produces a brand new
             // immutable state based off those changes.
             // Also, no return statement is required from these functions.
             if (payload) {
-                localStorage.setItem('token', payload)
-                state.isAuthenticated = localStorage.getItem('token')
+                state.isAuthenticated = payload
             }
         },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { setLogin } = login.actions
+export const { setUser } = user.actions
 
-export default login.reducer
+export default user.reducer
