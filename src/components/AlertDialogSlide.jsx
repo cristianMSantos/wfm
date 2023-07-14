1f136c4c6a42ede1aa869b43b053c8a1c34adc72
cristian_santos <cristian_santos@git>
2023-07-14 18:18:36 -0300

    update

---
+++ src/components/AlertDialogSlide.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function AlertDialogSlide({ open, setAlertDialog, setResultAlertDialog }) {
+
+    const handleCancel = () => {
+        setAlertDialog(false);
+        setResultAlertDialog(false)
+    };
+
+    const handleSave = () => {
+        setResultAlertDialog(true)
+        setAlertDialog(false);
+    };
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleCancel}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Tem certeza que deseja prosseguir?"}</DialogTitle>
+                {/* <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {messageAlertDialog}
+          </DialogContentText>
+        </DialogContent> */}
+                <DialogActions>
+                    <Button onClick={handleCancel}>Cancelar</Button>
+                    <Button onClick={handleSave}>Salvar</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
--- src/views/admin/Acessos.jsx
+++ src/views/admin/Acessos.jsx
@@ -1,35 +1,253 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
 import StepperAccess from "./StepperAccess";
 import { Card, CardContent, CardHeader, Avatar, IconButton } from "@mui/material";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Box } from "@mui/material";
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import CustomDataGrid from "../../components/CustomDataGrid";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import { useSelector } from "react-redux";
+import api from "../../axios";
+import Alert from "../../components/Alert";
+import moment from 'moment'
+
+import AlertDialogSlide from "../../components/AlertDialogSlide";
 
 export default function Acessos() {
     const theme = useTheme();
+    const token = useSelector((state) => state.login.isAuthenticated);
     const [valueTab, setValueTab] = React.useState('1');
+    const [rows, setRows] = React.useState([]);
+    const [rowModesModel, setRowModesModel] = React.useState({});
+    const [openAlert, setOpenAlert] = React.useState(false);
+    const [openAlertDialog, setAlertDialog] = React.useState(false);
+    const [resultAlertDialog, setResultAlertDialog] = React.useState(false);
+    const [alertType, setAlertType] = React.useState(undefined);
+    const [messageAlert, setMessageAlert] = React.useState('');
+    const [rowEditing, setRowEditing] = React.useState(undefined);
+
+
+    const getListAccess = async () => {
+        const options = {
+            url: `adm/listAccess`,
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                Authorization: token ? `Bearer ${token}` : "",
+            },
+        };
+
+        try {
+            let response = await api(options);
+            response = response.data.filter(e => {
+                if (e.dt_criacao) {
+                    return e.dt_criacao = moment(e.dt_criacao).format('DD/MM/YYYY');
+                }
+            })
+
+            setRows(response)
+        } catch (error) {
+            setAlertType('error')
+            setMessageAlert('Falha na busca de acessos!')
+            setOpenAlert(true)
+        }
+    }
+
+    const saveEdit = async (rowEditing) => {
+        const options = {
+            url: `adm/access/update`,
+            method: "PUT",
+            data: rowEditing,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                Authorization: token ? `Bearer ${token}` : "",
+            },
+        };
+
+        try {
+            const response = await api(options);
+            setAlertType('success');
+            setMessageAlert('Acesso atualizado!');
+            setOpenAlert(true);
+            setResultAlertDialog(false)
+        } catch (error) {
+            setAlertType('error')
+            setMessageAlert('Falha na atualização!')
+            setOpenAlert(true)
+        }
+    }
+
+    useEffect(() => {
+        getListAccess()
+    }, [])
 
     const handleChangeTab = (event, newValue) => {
         setValueTab(newValue);
     };
 
+    const onRowEditStart = ({ id }) => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: 'edit' } });
+    }
+
+    const handleEditClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: 'edit' } });
+    };
+
+    const handleSaveClick = (id, row) => () => {
+        setRowEditing(row)
+        setAlertDialog(true)
+    };
+
+    const handleRowModesModelChange = (newRowModesModel) => {
+        setRowModesModel(newRowModesModel);
+      };    
+
+    useEffect(() => {
+        console.log(resultAlertDialog)
+        if (resultAlertDialog) {
+            console.log('SALVAR');
+            setRowModesModel({
+                ...rowModesModel,
+                [rowEditing.id]: { mode: 'view' },
+            });
+            //   (async () => {
+            //      saveEdit(rowEditing)
+            //   })()
+        }
+    }, [resultAlertDialog]);
+
+    const handleDeleteClick = (id) => () => {
+        console.log('handleDeleteClick')
+    };
+
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: 'view', ignoreModifications: true },
+        });
+
+        setResultAlertDialog(false)
+
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow.isNew) {
+            setRows(rows.filter((row) => row.id !== id));
+        }
+    };
+
+    const processRowUpdate = (newRow) => {
+        const updatedRow = { ...newRow, isNew: false };
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        saveEdit(newRow)
+        return updatedRow;
+    };
+
+
+    const columns = [
+        {
+            field: 'no_operador',
+            headerName: 'Nome',
+            width: 180,
+        },
+        {
+            field: 'matricula',
+            headerName: 'Matricula',
+            // type: 'number',
+            align: 'left',
+            headerAlign: 'left',
+        },
+        {
+            field: 'co_perfil',
+            headerName: 'Perfil',
+            width: 180,
+            editable: true,
+            type: 'singleSelect',
+            valueOptions: [
+                { value: 1, label: 'ADMINISTRADOR' },
+                { value: 2, label: 'RECRUTAMENTO' },
+            ],
+        },
+        {
+            field: 'mat_criacao',
+            headerName: 'Liberado Por',
+            width: 220,
+        },
+        {
+            field: 'dt_criacao',
+            headerName: 'Data de Registro',
+            // type: 'date',
+            width: 180,
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id, row }) => {
+                const isInEditMode = rowModesModel[id]?.mode === 'edit';
+                if (isInEditMode) {
+                    return [
+                        <IconButton
+                            label="Save"
+                            sx={{
+                                color: 'primary.main',
+                            }}
+                            onClick={handleSaveClick(id, row)}
+                        >
+                            <SaveIcon />
+                        </IconButton>,
+                        <IconButton
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        >
+                            <CancelIcon />
+                        </IconButton>,
+                    ];
+                }
+
+                return [
+                    <IconButton
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    >
+                        <EditIcon />
+                    </IconButton>,
+                    <IconButton
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    >
+                        <DeleteIcon />
+                    </IconButton>,
+                ];
+            },
+        },
+    ];
+
     return (
-        <Box>
-            <Card>
-                <TabContext value={valueTab}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChangeTab} aria-label="lab API tabs example" centered>
-                            <Tab label="Cadastro de Acessos" value="1" />
-                            <Tab label="Listagem de Acessos " value="2" />
-                        </TabList>
-                    </Box>
-                    <TabPanel value="1">
-                        {/* <CardHeader
+        <div>
+
+            <Box>
+                <Card>
+                    <TabContext value={valueTab}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChangeTab} aria-label="lab API tabs example" centered>
+                                <Tab label="Cadastro de Acessos" value="1" />
+                                <Tab label="Listagem de Acessos " value="2" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">
+                            {/* <CardHeader
                             avatar={
                                 <Avatar aria-label="recipe">
                                     R
@@ -42,15 +260,30 @@ export default function Acessos() {
                             }
                             title="Cadastro de Acessos">
                         </CardHeader> */}
-                        <CardContent>
-                            <StepperAccess />
-                        </CardContent>
-                    </TabPanel>
-                    <TabPanel value="2">
-                        <CustomDataGrid />
-                    </TabPanel>
-                </TabContext>
-            </Card>
-        </Box>
+                            <CardContent>
+                                <StepperAccess />
+                            </CardContent>
+                        </TabPanel>
+                        <TabPanel value="2">
+                            <CustomDataGrid
+                                rows={rows}
+                                columns={columns}
+                                rowModesModel={rowModesModel}
+                                processRowUpdate={processRowUpdate}
+                                onRowEditStart={onRowEditStart}
+                                handleRowModesModelChange={handleRowModesModelChange} />
+                        </TabPanel>
+                    </TabContext>
+                </Card>
+            </Box>
+            <Box>
+                <Alert open={openAlert} type={alertType} setOpenAlert={setOpenAlert} messageAlert={messageAlert} />
+                <AlertDialogSlide
+                    open={openAlertDialog}
+                    setAlertDialog={setAlertDialog}
+                    setResultAlertDialog={setResultAlertDialog}
+                />
+            </Box>
+        </div>
     )
 }
